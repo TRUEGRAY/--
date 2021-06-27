0aeb5ad0a74270e19980ed5955f7815611d66891
YOULIANG <56952404+TRUEGRAY@users.noreply.github.com>
2021-06-27 19:07:36 +0800

    Add files via upload

---
+++ olu.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 27 16:03:16 2021
+
+@author: CAO YOULIANG
+"""
+
+import requests
+import re
+import time
+#主要爬取用户在欧路词典收藏的生僻词：
+
+session = requests.session()
+data = {
+#昵称密码：
+            'UserName':'***',
+            'Password':'***'
+        }
+#post请求登录：
+url = "http://dict.eudic.net/Account/Login"
+session.post(url,data = data)
+
+#数据爬取列表1—70：
+for i in range(70):
+    a = i + 1
+    b = i*25
+    c = 1624790670399+a;
+    url1 = 'https://my.eudic.net/StudyList/WordsDataSource?draw='+ str(a) +'&columns%5B0%5D%5Bdata%5D=id&columns%5B0%5D%5Bname%5D=&columns%5B0%5D%5Bsearchable%5D=false&columns%5B0%5D%5Borderable%5D=false&columns%5B0%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B0%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B1%5D%5Bdata%5D=id&columns%5B1%5D%5Bname%5D=&columns%5B1%5D%5Bsearchable%5D=true&columns%5B1%5D%5Borderable%5D=false&columns%5B1%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B1%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B2%5D%5Bdata%5D=word&columns%5B2%5D%5Bname%5D=&columns%5B2%5D%5Bsearchable%5D=true&columns%5B2%5D%5Borderable%5D=true&columns%5B2%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B2%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B3%5D%5Bdata%5D=phon&columns%5B3%5D%5Bname%5D=&columns%5B3%5D%5Bsearchable%5D=true&columns%5B3%5D%5Borderable%5D=false&columns%5B3%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B3%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B4%5D%5Bdata%5D=exp&columns%5B4%5D%5Bname%5D=&columns%5B4%5D%5Bsearchable%5D=true&columns%5B4%5D%5Borderable%5D=false&columns%5B4%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B4%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B5%5D%5Bdata%5D=rating&columns%5B5%5D%5Bname%5D=&columns%5B5%5D%5Bsearchable%5D=false&columns%5B5%5D%5Borderable%5D=true&columns%5B5%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B5%5D%5Bsearch%5D%5Bregex%5D=false&columns%5B6%5D%5Bdata%5D=addtime&columns%5B6%5D%5Bname%5D=&columns%5B6%5D%5Bsearchable%5D=false&columns%5B6%5D%5Borderable%5D=true&columns%5B6%5D%5Bsearch%5D%5Bvalue%5D=&columns%5B6%5D%5Bsearch%5D%5Bregex%5D=false&order%5B0%5D%5Bcolumn%5D=2&order%5B0%5D%5Bdir%5D=asc&start='+str(b)+'&length=25&search%5Bvalue%5D=&search%5Bregex%5D=false&categoryid=studyList&_='+str(c)
+#session请求：
+    resp = session.get(url1)
+    obj = re.compile(r'''"uuid":"(?P<word>.*?)"''', re.S)
+    it = obj.finditer(resp.text)
+    with open("test.txt","a") as f:
+        for i in it:
+            f.writelines(i.group("word") + '\n')
+    print(a)
+    time.sleep(5)
+
+    
